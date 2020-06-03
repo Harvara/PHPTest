7c404e23dfa9e02ef04435320f509b05af2f2fa5
Henry Navara <hart.hunter76@gmail.com>
2020-06-03 15:26:05 +0200

    Refactors Constructors

--- main.php
+++ main.php
@@ -29,9 +29,28 @@ $result = $statement->fetchAll(PDO::FETCH_ASSOC);
 foreach ($result as $res){
     array_push(
         $messages,
-        new Message($res)
+        Message::withContent($res)
     );
 }
+
+
+$f = fopen("php://stdin","r");
+
+echo "Create new Message".PHP_EOL;
+
+echo "Enter Content:" . PHP_EOL;
+$messageContent = fgets($f);
+$messageContent=rtrim($messageContent, "\r\n");
+
+echo "Enter Sender IP:" . PHP_EOL;
+$messageSender = fgets($f);
+$messageSender=rtrim($messageSender, "\r\n");
+
+array_push($messages, Message::withContentAndSender($messageContent,$messageSender));
+
+fclose($f);
+
+
 
 foreach ($messages as $message){
     $message->printMessage();
--- src/classes/Message.php
+++ src/classes/Message.php
@@ -11,26 +11,61 @@ class Message {
     public static $defaultContent = "This is the message Content";
     const MESSAGE_SERVER = "147.9.2.3";
 
-    public function __construct($content=false){
+    public  function  __construct(){
+
+    }
+
+    public function withContentAndSender($content = false, $sender){
+        $instance= new self();
         if (!$content){
             $content = Message::$defaultContent;
         }
         if(gettype($content)==="array"){
-            $this->createNewMessagefromArray($content);
+            $members = $this->getAttributesfromDBQuery($content);
         }else{
-            $this->content=$content;
+            $members["content"]=$content;
         }
+        $members["sender"]=$sender;
+        $instance->fill($members);
+        return $instance;
+    }
+
+    public function withContent($content=false){
+        $instance = new self();
+        if (!$content){
+            $content = Message::$defaultContent;
+        }
+        if(gettype($content)==="array"){
+            $members = $instance->getAttributesfromDBQuery($content);
+        }else{
+            $members["content"]=$content;
+            $members["sender"]="127.0.0.1";
+        }
+        $instance->fill($members);
+        return $instance;
+    }
+
+
+    private function  getAttributesfromDBQuery($result) {
+        $members =[];
+        if(sizeof($result)>0) {
+            $members["content"] = $result["Content"];
+            $members["sender"] = $result["SenderIP"];
+        }
+        echo "MEmber Content: " . $members["content"].PHP_EOL;
+        return $members;
+    }
+
+    private  function fill($members){
+        $this->content=$members["content"];
+        $this->sender=$members["sender"];
         $this->meta= new MessageMeta("127.0.0.1","AF:00:00:00");
     }
 
-    private function  createNewMessagefromArray($content){
-        if (sizeof($content)>0){
-            $this->content=$content["Content"];
-            $this->sender=$content["SenderIP"];
-        }else{
-            $this->content=Message::$defaultContent;
-        }
-    }
+
+
+
+
 
     public function setMessage($content){
         $this->content=$content;
