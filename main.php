7ba652f279c65b0daabda46ac94394aebb541bd1
Henry Navara <hart.hunter76@gmail.com>
2020-06-03 13:33:21 +0200

    Queries with results

--- main.php
+++ main.php
@@ -13,3 +13,36 @@ try {
 } catch (PDOException $e) {
     die("SLQ Connection Error" . $e);
 }
+
+
+$sql="select * from Messages";
+$statement = $pdo->prepare($sql);
+
+
+$statement->execute();
+$result = $statement->fetch(PDO::FETCH_ASSOC);
+echo "Content:" . $result["Content"] . PHP_EOL;
+
+
+
+$sql="select * from Messages where SenderIP= :sip";
+$statement = $pdo->prepare($sql);
+
+$sip= "127.0.0.1";
+$statement->bindValue(":sip",$sip);
+
+$statement->execute();
+$result = $statement->fetch(PDO::FETCH_ASSOC);
+echo "Content:" . $result["Content"] . PHP_EOL;
+
+
+$sql="select * from Messages where ID = :id";
+$statement = $pdo->prepare($sql);
+
+$statement->bindValue(":id",1,PDO::PARAM_INT);
+
+
+$statement->execute();
+$result = $statement->fetch(PDO::FETCH_ASSOC);
+echo "Content:" . $result["Content"] . PHP_EOL;
+
